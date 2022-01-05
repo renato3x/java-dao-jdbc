f17526fff94561a4b08ed1d968d5bddab327a955
renato3x <pereirarenato21@gmail.com>
2022-01-05 14:41:26 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
   public static void main(String[] args) {
     Department dep = new Department(1, "Books");
+    Seller seller = new Seller(1, "oiafo", "email", new Date(), 300.0, dep);
 
     System.out.println(dep);
+    System.out.println(seller);
   }
 }
